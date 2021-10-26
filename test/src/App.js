e19e5f8532f22a3c02a9dcae9a8ce0808288d056
Eric <uwizeyeeric@icloud.com>
2021-10-26 20:57:21 +0800

    update Test

--- test/src/App.js
+++ test/src/App.js
@@ -2,6 +2,9 @@ import { lazy, Suspense, useState } from "react";
 import { Hidden, Drawer, Box } from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 const Bar = lazy(() => import("./components/Bar"));
+const Browse = lazy(() => import("./components/Browse"));
+const Title = lazy(() => import("./components/Title"));
+const Maps = lazy(() => import("./components/ChinaMap"));
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -21,13 +24,32 @@ function App(props) {
     setMobileOpen(!mobileOpen);
   };
 
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
   return (
     <div>
       <Suspense fallback={<div></div>}>
         <Bar handleDrawerToggle={handleDrawerToggle} />
-        <div style={{
-          height: '600vh'
-        }}></div>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+            },
+          }}
+        >
+          <Browse />
+        </Drawer>
+        <Title />
+        <Maps />
       </Suspense>
     </div>
   );
---
+++ test/src/components/Card.js
@@ -0,0 +1,74 @@
+import React, { forwardRef } from "react";
+import { makeStyles } from "@material-ui/styles";
+import Fade from "@material-ui/core/Fade";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    position: "absolute",
+    width: "400px",
+    height: "130px",
+    backgroundColor: "white",
+    boxShadow: "0 10px 25px rgba(92, 99, 105, .2)",
+    display: "flex",
+    flexDirection: "row",
+    userSelect: "none",
+    transition: "all 1s",
+  },
+  cardContent: {
+    height: "100%",
+    width: "70%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cardImg: {
+    height: "100%",
+    width: "30%",
+    // backgroundImage: "url(Images/Kido/Logo/Kido.png)",
+    backgroundSize: "70%",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+  },
+  text: {
+    margin: "0",
+  },
+  founder: {
+    color: "rgb(165, 36, 56)",
+  },
+  placeName: {
+    color: "rgb(87, 91, 102)",
+  },
+}));
+
+function Card({ card }, ref) {
+  const classes = useStyles();
+  const { founder, position, vision, logo } = card.properties;
+  const { province, city } = position;
+  return (
+    <Fade in={card.visibility} timeout={1000}>
+      <div
+        ref={ref}
+        className={classes.root}
+        style={{
+          top: card.top + "px",
+          left: card.left + "px",
+        }}
+      >
+        <div className={classes.cardContent}>
+          <h2 className={classes.text}>{vision}</h2>
+          <p className={classes.text}>
+            <span className={classes.founder}>{founder}</span>
+            <span className={classes.placeName}>, {province}{city}</span>
+          </p>
+        </div>
+        <div 
+          className={classes.cardImg}
+          style={{ backgroundImage: 'url(' + logo + ')'}}
+        ></div>
+      </div>
+    </Fade>
+  );
+}
+
+export default forwardRef(Card);
