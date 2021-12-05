6f39a9f2fd96358d1f29d0a93665d740a201158e
Eric <uwizeyeeric@icloud.com>
2021-12-06 00:21:00 +0800

    The feature of the "Drawer" is being improved

--- react-app/src/components/Browse.js
+++ react-app/src/components/Browse.js
@@ -1,24 +1,44 @@
-import React from "react";
-import PropTypes from "prop-types";
-import SwipeableViews from "react-swipeable-views";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import SwipeableViews from 'react-swipeable-views';
 import {
   Box,
   AppBar,
   Tabs,
-  Button
-} from "@material-ui/core";
-import { makeStyles } from "@material-ui/styles";
+  Button,
+  Tab,
+  Typography,
+  Toolbar,
+  List,
+  ListItem,
+  ListItemText,
+  SvgIcon,
+  ListItemIcon
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
+import { useSelector } from 'react-redux';
 // import Project from "./Bar/Project";
 
 // * ----------------------------------------------------------------
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: "100vw",
-    maxWidth: "400px",
-  },
+    width: '100vw',
+    maxWidth: '400px',
+    height: '100vh',
+    position: 'relative'
+  }
 }));
 
 // * ----------------------------------------------------------------
+
+function ProjectIcon({ url, ...other }) {
+  return (
+    <SvgIcon {...other}>
+      <image xlinkHref={url} height="25" width="25" />
+    </SvgIcon>
+  );
+}
+
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
 
@@ -26,12 +46,12 @@ function TabPanel(props) {
     <div
       role="tabpanel"
       hidden={value !== index}
-      id={`full-width-tabpanel-${index}`}
-      aria-labelledby={`full-width-tab-${index}`}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
       {...other}
     >
       {value === index && (
-        <Box sx={{ p: 3 }}>
+        <Box sx={{ p: 2 }}>
           <Box>{children}</Box>
         </Box>
       )}
@@ -42,69 +62,104 @@ function TabPanel(props) {
 TabPanel.propTypes = {
   children: PropTypes.node,
   index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired
 };
 
-function Browse() {
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`
+  };
+}
+
+function Browse({ handleDrawerToggle }) {
   const classes = useStyles();
   const theme = useStyles();
-  const [value, setValue] = React.useState(0);
+  const [value, setValue] = useState(0);
+  const { publicWelfareData: data } = useSelector(
+    (state) => state.publicWelfare
+  );
+
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
-  const handleChangeIndex = (index) => {
-    setValue(index);
+  const handleClick = (projectId, event) => {
+    const anchor = (event.target.ownerDocument || document).querySelector(
+      `#${projectId}`
+    );
+    if (anchor) {
+      anchor.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center'
+      });
+    }
+    // handleDrawerToggle();
   };
-
   return (
     <Box className={classes.root}>
-      <Button>关闭</Button>
-      <AppBar
-        position="static"
-        sx={{
-          width: "100%",
-          maxWidth: "880px",
-          bgcolor: "background.paper",
-          justifyContent: "center",
-        }}
-      >
-        <Tabs
-          // variant="fullWidth"
-          value={value}
-          onChange={() => handleChange()}
-          // indicatorColor="secondary"
-          // textColor="inherit"
-          // variant="scrollable"
-          // aria-label="scrollable auto tabs example"
-          // scrollButtons="auto"
-        >
-          {/* <Tab label="公益项目" {...a11yProps(0)} />
-          <Tab label="地点" {...a11yProps(1)} />
-          <Tab label="Purpose" {...a11yProps(2)} /> */}
-        </Tabs>
-      </AppBar>
-      <SwipeableViews
-        axis={theme.direction === "rtl" ? "x-reverse" : "x"}
-        index={value}
-        onChangeIndex={() =>handleChangeIndex()}
-      >
-        <TabPanel value={value} index={0} dir={theme.direction}>
-          <Box>
-            {/* <List>
-              {places.map(({ name, properties }, index) => (
-                <Project key={index} name={name} properties={properties} />
-              ))}
-            </List> */}
-          </Box>
+      {/* <Button style={{ position: 'fixed' }} onClick={handleDrawerToggle}>
+        关闭
+      </Button> */}
+
+      <Box sx={{ bgcolor: 'background.paper', width: 400 }}>
+        <AppBar position="static">
+          <Tabs value={value} onChange={handleChange}>
+            <Tab label="项目" {...a11yProps(0)} />
+            <Tab label="地点" {...a11yProps(1)} />
+            <Tab label="目的" {...a11yProps(2)} />
+          </Tabs>
+        </AppBar>
+        <TabPanel value={value} index={0}>
+          <List
+            sx={{
+              width: '100%',
+              maxWidth: 360,
+              bgcolor: 'white'
+            }}
+            component="nav"
+          >
+            {data.map(({ name, properties }) => {
+              return (
+                <ListItem button onClick={(event) => handleClick(name, event)}>
+                  <ListItemIcon>
+                    <ProjectIcon url={properties.logo} />
+                  </ListItemIcon>
+                  <ListItemText>{name}</ListItemText>
+                </ListItem>
+              );
+            })}
+          </List>
         </TabPanel>
-        <TabPanel value={value} index={1} dir={theme.direction}>
+        <TabPanel value={value} index={1}>
           Item Two
         </TabPanel>
-        <TabPanel value={value} index={2} dir={theme.direction}>
-          Item Three
+        <TabPanel value={value} index={2}>
+          <List component="nav">
+            <ListItem button>
+              <ListItemText>Empower</ListItemText>
+            </ListItem>
+            <ListItem button>
+              <ListItemText>Create</ListItemText>
+            </ListItem>
+            <ListItem button>
+              <ListItemText>Respond</ListItemText>
+            </ListItem>
+            <ListItem button>
+              <ListItemText>Improve</ListItemText>
+            </ListItem>
+          </List>
         </TabPanel>
-      </SwipeableViews>
+      </Box>
+      {/* <Box
+        style={{
+          position: 'fixed',
+          height: '60px',
+          width: '100%',
+          maxWidth: '400px',
+          bottom: 0
+        }}
+      ></Box> */}
     </Box>
   );
 }
